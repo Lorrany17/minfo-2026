a365406def8c46b6a58e2db3c762d3d1b3a3aa1a
Lorrany <santoslorrany250@gmail.com>
2026-01-16 13:43:19 -0300

    Corrigindo título e descrição do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter } from "next/font/google"; 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// AQUI ESTÁ O SEGREDO ⬇️
 export const metadata: Metadata = {
-  title: "5ª MINFO",
-  description: "A Grandona",
+  title: "5ª MINFO | IFPI Campus Picos",
+  description: "Participe da 5ª Mostra de Informática do IFPI Picos. Inovação, tecnologia e projetos incríveis dos alunos de Informática e ADS.",
+  
+  openGraph: {
+    title: "5ª MINFO - 5 Anos de Inovação",
+    description: "Conheça os projetos da Mostra de Informática do IFPI Campus Picos.",
+    locale: 'pt_BR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
